dfaf181441ccb3a318887f7f1cc2394f7d70b641
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-20 18:38:07 -0500

    update user, place and city

--- models/place.py
+++ models/place.py
@@ -8,7 +8,7 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
@@ -17,4 +17,4 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = Column(String(60), ForeignKey('amenities.id'), nullable=False)
+    amenity_ids = ''
