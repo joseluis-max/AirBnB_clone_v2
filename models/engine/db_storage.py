f21dde0da1ee92c8d234e4f83a3da5cdbf0891ed
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-20 17:25:38 -0500

    DBStorage - States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+""" New engine storage 
+"""
+import sqlalchemy
+from os import getenv
+# from dotenv import load_dotenv
+from models.base_model import Base
+from models.city import City
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+class DBStorage():
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """ create the engine (self.__engine)
+        """
+        # load_dotenv()
+        URL = 'mysql+mysqldb://{:s}:{:s}@{:s}:3306/{:s}'.format(getenv('HBNB_MYSQL_USER'),
+                                                                getenv('HBNB_MYSQL_PWD'),
+                                                                getenv('HBNB_MYSQL_HOST'),
+                                                                getenv('HBNB_MYSQL_DB'))
+        self.__engine = sqlalchemy.create_engine(URL, pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+    
+    def all(self, cls=None):
+        """ query on the current database session (self.__session)
+            all objects depending of the class name (argument cls)
+        """
+        all_cls = [State, City, User, Place, Review, Amenity]
+
+        cls_dict = {}
+        objs = []
+
+        if cls:
+            objs = self.__session.query(cls).all()
+
+        else:
+            for cls in all_cls:
+                objs += self.__session.query(cls)
+
+        for obj in objs:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            cls_dict[key] = obj
+
+        return cls_dict
+
+    def new(self, obj):
+        """ query on the current database session (self.__session)
+            all objects depending of the class name (argument cls)
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """ commit all changes of the current database session (self.__session)
+        """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ delete from the current database session obj if not None    
+        """
+        self.__session.delete(obj)
+
+    def reload(self):
+        """
+            create all tables in the database (feature of SQLAlchemy) (WARNING:
+                all classes who inherit from Base must be imported before
+                calling Base.metadata.create_all(engine))
+            create the current database session
+                (self.__session) from the engine
+                (self.__engine) by using a sessionmaker -
+                the option expire_on_commit must be set to False ; and scoped_session -
+                to make sure your Session is thread-safe
+        """
+        from models import state, city, base_model
+        from sqlalchemy.orm import sessionmaker
+        from sqlalchemy.orm import scoped_session
+
+        Base.metadata.create_all(self.__engine)
+        Session = scoped_session(sessionmaker(bind=self.__engine, expire_on_commit=False))
+        self.__session = Session()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('DBStorage', cascade='all, delete', backref='state')
+    
+    def cities(self):
+        """getter attribute cities that returns the list of City
+            instances with state_id equals to the current State.id => It will be the FileStorage
+            relationship between State and City
+        """
+        from models import storage
+        cities = []
+        
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                cities.append(city)
+        return cities
