407d5a8d76363f1f343ec5417aea7ac7ad291131
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-13 12:33:32 -0500

    ADD: setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,5 +8,5 @@ sudo mkdir -p /data/web_static/shared/
 echo "<h1>Html Testing Server</h1>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sudo sed -i "/^\tlocation \/ {$/ i\\\tlocation /\hbnb_static { \n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n}\n" /etc/nginx/sites-available/default
+sudo sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
