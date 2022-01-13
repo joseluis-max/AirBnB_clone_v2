66dc7a7c0ce0ef006342cdf5ea05c7414418580a
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-13 12:27:49 -0500

    ADD: setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,11 +3,10 @@
 sudo apt -y update
 sudo apt -y install nginx
 ufw allow 'Nginx HTTP'
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo "<h1>Html Testing Server</h1>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -Rh ubuntu /data
-chgrp -Rh ubuntu /data
-sed -i "/^\tlocation \/ {$/ i\\\tlocation /\hbnb_static { \n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n}\n" /etc/nginx/sites-available/default
+chown -R ubuntu:ubuntu /data/
+sudo sed -i "/^\tlocation \/ {$/ i\\\tlocation /\hbnb_static { \n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
