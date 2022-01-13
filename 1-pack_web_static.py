529dd0f36f8498ac0c2063146c9ea69f59c8ad26
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-13 16:48:00 -0500

    UPDATE: return by failed attr, and use format

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,10 +2,12 @@
 """ Generate .tgz
 """
 from fabric.api import local
-from datetime import date, datetime
+from datetime import datetime
 
 
 def do_pack():
+    """ Genarate .tgz from web_static with fabric
+    """
     now = datetime.now()
     date = now.strftime("%Y%m%d%H%S%M")
     status = local("tar -cvzf versions/web_static_{}.tgz web_static".format(date))
