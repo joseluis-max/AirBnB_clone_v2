605ab9ef90ee8dbe4cbf777b3af832f996f9131c
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-13 18:03:27 -0500

    UPDATE: name file path

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 """ Deploy a static content
 """
 from fabric.api import put, run, env
-env.host['34.75.115.12', "54.92.203.11"]
+env.hosts = ['34.75.115.12', '54.92.203.11']
 
 
 def do_deploy(archive_path):
@@ -16,24 +16,24 @@ def do_deploy(archive_path):
             # Uncompress the archive to the folder
             # / data/web_static/releases/<archive filename without extension
             #  > on the web server
-            path = archive_path.split("/")[1]
-            name_file = path.split(".")[0]
-            run("mkdir -p /data/web_static/releases/{}/".format(path))
+            path = archive_path.split("/")
+            name_file = path[1].split(".")[0]
+            run("mkdir -p /data/web_static/releases/{}/".format(name_file))
             run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-                .format(archive_path, path))
+                .format(path[1], name_file))
             # Delete the archive from the web server
-            run("rm /tmp/{}".format(archive_path))
+            run("rm /tmp/{}".format(path[1]))
             run("mv /data/web_static/releases/{}/web_static/*\
                  /data/web_static/releases/{}/"
-                .format(path, path))
+                .format(name_file, name_file))
             # Delete the symbolic link /data/web_static/current
             # from the web server
             run("rm -rf /data/web_static/releases/{}/web_static"
-                .format(path))
+                .format(name_file))
             run("rm -rf /data/web_static/current")
             run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-                .format(path))
-
+                .format(name_file))
+            print("New version deployed!")
             return True
         else:
             return False
