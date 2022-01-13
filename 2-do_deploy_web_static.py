1524ea60f0ab1e4fa561d5f3645f6bbf1c83c86c
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-13 17:39:05 -0500

    UPDATE: pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,22 +10,29 @@ def do_deploy(archive_path):
     """Deploy a static content in the web server
     """
     if archive_path:
-        #Upload the archive to the /tmp/ directory of the web server
+        # Upload the archive to the /tmp/ directory of the web server
         status = put(archive_path, "/tmp")
         if status.succeeded:
-            # Uncompress the archive to the folder 
-            # / data/web_static/releases/<archive filename without extension > on the web server
+            # Uncompress the archive to the folder
+            # / data/web_static/releases/<archive filename without extension
+            #  > on the web server
             path = archive_path.split("/")[1]
             name_file = path.split(".")[0]
             run("mkdir -p /data/web_static/releases/{}/".format(path))
-            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_path, path))
+            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+                .format(archive_path, path))
             # Delete the archive from the web server
             run("rm /tmp/{}".format(archive_path))
-            run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(path, path))
-            # Delete the symbolic link /data/web_static/current from the web server
-            run("rm -rf /data/web_static/releases/{}/web_static".format(path))
+            run("mv /data/web_static/releases/{}/web_static/*\
+                 /data/web_static/releases/{}/"
+                .format(path, path))
+            # Delete the symbolic link /data/web_static/current
+            # from the web server
+            run("rm -rf /data/web_static/releases/{}/web_static"
+                .format(path))
             run("rm -rf /data/web_static/current")
-            run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(path))
+            run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+                .format(path))
 
             return True
         else:
