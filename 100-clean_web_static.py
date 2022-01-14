f683ed25c417afd7b24abae9b146db61ec3fa8d3
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-13 21:24:28 -0500

    ADD: clean files

---
+++ 100-clean_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Clean files oldest
+"""
+from fabric.api import run, local, env
+env.hosts = ['34.75.115.12', '54.92.203.11']
+
+
+def do_clean(number=0):
+    """
+    """
+    if number == 0 or number == 1:
+        number = 2
+    else:
+        number = int(number) + 1
+    local("""ls -t versions | tail +{} |
+           sed 's/^/versions\//' | xargs -d '\n' rm"""
+          .format(number))
+    run("""ls -t /data/web_static/releases | tail +{} |
+         sed 's/^/data/web_static/releases\//' | xargs -d '\n' rm"""
+        .format(number))
