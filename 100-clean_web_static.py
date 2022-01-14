0ac3b94c2d947071dcbd74fc9d66b48a17d1c2ff
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-14 10:03:28 -0500

    UPDATE: clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Clean files oldest
 """
-from fabric.api import run, local, env
+from fabric.api import run, local, env, cd
 env.hosts = ['34.75.115.12', '54.92.203.11']
 
 
@@ -13,9 +13,10 @@ def do_clean(number=0):
     else:
         number = int(number) + 1
     # Trying with cd operations.
-    local("""ls -t versions | tail +{} |
-           sed 's/^/versions\//' | xargs -d '\n' rm"""
-          .format(number))
-    run("""ls -t /data/web_static/releases | tail +{} |
-         sed 's/^/data/web_static/releases\//' | xargs -d '\n' rm"""
-        .format(number))
+    with cd('/versions'):
+        local("ls -t . | tail +{} | xargs -d '\n' rm"
+             .format(number))
+
+    with cd('/data/web_static/releases'):
+        run("ls -t . | tail +{} | xargs -d '\n' rm"
+           .format(number))
