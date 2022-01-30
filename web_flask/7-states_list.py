d08f1f0b5181a44d06009029e5f88347c3834050
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-30 11:33:26 -0500

    ADD: render states list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,17 +5,17 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_appcontext(exc):
-    """ Remove the current SQLAlchemy Session"""
-    storage.close()
-
-
 @app.route('states_list', strict_slashes=False)
 def states_list():
     """ Return list of states """
     data = storage.all("State")
     return render_template('7-states_list.html', data=data)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exc):
+    """ Remove the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == '__main__':
